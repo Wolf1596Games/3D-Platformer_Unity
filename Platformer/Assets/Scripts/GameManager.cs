c5025f32ce9f78ff20da1024d0efb9ca99480949
Wolf1596Games <wolf1596games@gmail.com>
2020-09-11 11:30:57 -0500

    Added items and GameManager
    - Added item counter
    - Items are destroyed when the player collides with them

---
+++ Platformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int itemsRemaining;
+
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CountItems()
+    {
+        itemsRemaining++;
+    }
+}
---
+++ Platformer/Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    GameManager gameManager;
+
+    private void Start()
+    {
+        CountItemsRemaining();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void CountItemsRemaining()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        if(tag == "Item")
+        {
+            gameManager.CountItems();
+        }
+    }
+}
